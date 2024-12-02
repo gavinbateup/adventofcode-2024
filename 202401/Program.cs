6c4b63a85678dd10841652eae8a71b6f0e939c8b
Gavin Bateup <gavin.bateup@127.co.nz>
2024-12-02 13:36:19 +1300

    Day 1

---
+++ 202401/Program.cs
@@ -0,0 +1,35 @@
+﻿var input = File.ReadAllText("./Input.txt");
+
+var lines = input.Split('\n');
+var left = new List<int>();
+var right = new List<int>();
+foreach (var line in lines)
+{
+    var values = line.Split(' ', StringSplitOptions.RemoveEmptyEntries );
+    left.Add(int.Parse(values[0]));
+    right.Add(int.Parse(values[1]));
+}
+
+left = left.OrderBy(x => x).ToList();
+right = right.OrderBy(x => x).ToList();
+
+// part 1
+long total = 0;
+for (int i = 0; i < left.Count; i++)
+{
+    total += Math.Abs(right[i] - left[i]);
+}
+
+Console.WriteLine($"Part 1: {total}");
+
+
+// part 2
+
+
+long total2 = 0;
+for (int i = 0; i < left.Count; i++)
+{
+    total2 += left[i] * right.Count(x => x == left[i]) ;
+}
+
+Console.WriteLine($"Part 2: {total2}");
